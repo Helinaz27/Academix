a687ae47e63743f0cafa092413522827a7e570de
BeamSol <141314118+BeamSol@users.noreply.github.com>
2024-03-30 20:37:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ModeratorApplication from './component/ModeratorApplication.jsx';
+import Footer from './component/Footer.jsx';
 function App() {
   return (
     <div className="App">
       <ModeratorApplication />
+      <Footer />
     </div>
   );
 }
