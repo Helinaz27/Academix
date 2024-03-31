39a03798d9ae8fe6bb7c1fb5c1b211f04273ae92
abdiaziz34 <abdiaziz.jamal@aastustudent.edu.et>
2024-03-31 17:20:23 +0300

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ModeratorApplication from './component/ModeratorApplication.jsx';
 import Footer from './component/Footer.jsx';
+import Hero from './component/Hero.jsx';
 function App() {
   return (
     <div className="App">
+      <Hero />
       <ModeratorApplication />
       <Footer />
     </div>
