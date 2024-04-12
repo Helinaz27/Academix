bbb29ee9d6fb79cca2ac1ed4ecccb10a6164445a
abelsileshie <sileshieabel@gmail.com>
2024-04-12 16:56:29 +0300

    New-Folder-Structure

---
+++ src/component/Student/General-post/Gpost.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AdminNav from "../Admin/AdminNav";
+import Posts from "./Posts";
+import TrendingPostcard from "../General-post/TrendingPostcard";
+import Postfilter from "./Postfilter";
+
+const Gpost = () => {
+  return (
+    <>
+      {/* <TrendingPostcard /> */}
+      <Posts />
+      <Postfilter />
+    </>
+  );
+};
+
+export default Gpost;
