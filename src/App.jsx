68bea7fa67be6c879a32fc418028f16b6502f975
Abel S <150824348+AbelSileshie@users.noreply.github.com>
2024-04-12 15:38:48 +0300

    Merge conflict App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,14 @@ import Signup from "./pages/Home/Signup";
 import Navigation from "./component/Home/Navigation";
 import Footer from "./component/General/Footer";
 import User from "./component/Admin/users/User.jsx";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Studentdashboard from "./component/Student/Dashboard/Studentdashboard.jsx";
-
-=======
+// import Studentdashboard from "./component/Student/Dashboard/Studentdashboard.jsx";
 import Student from "./component/Student/Dashboard/Studentdashboard.jsx";
 import Gpost from "./component/General-post/Gpost.jsx";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "./Features/auth/authSlice.js";
 import Redirector from "./component/General/Redirector.jsx";
->>>>>>> Abel
-=======
 import Studentdashboard from "./component/Student/Dashboard/Studentdashboard.jsx";
 import StudentNav from "./component/Student/StudentNav.jsx";
->>>>>>> 06e312e43af3e6afac7b04263f5294469a6ea77b
 
 function App() {
   const user = useSelector(selectCurrentUser);
@@ -33,20 +26,9 @@ function App() {
     <div className="">
       <main></main>
       <Routes>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> 06e312e43af3e6afac7b04263f5294469a6ea77b
         <Route index element={<Studentdashboard />} />
         <Route index element={<Signup />} />
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
         <Route index element={<Home />} />
->>>>>>> Abel
-=======
->>>>>>> 06e312e43af3e6afac7b04263f5294469a6ea77b
         <Route path="/Home" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
